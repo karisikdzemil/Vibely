c9da17b037fe8e335b34d3b92b9e7e4ed82b3f4b
dzemil karisik <karisikdzemil@gmail.com>
2025-05-22 20:13:42 +0200

    displayed followers

--- src/components/profile/ProfileActionInfo.jsx
+++ src/components/profile/ProfileActionInfo.jsx
@@ -1,46 +1,59 @@
 import { useEffect, useState } from "react"
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
-import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+// import { useSelector } from "react-redux";
 
-export default function ProfileActionInfo () {
-    const [posts, setPosts] = useState([]);
-    
-    const user = useSelector(state => state.user.user);
+export default function ProfileActionInfo() {
+  const [posts, setPosts] = useState([]);
+  const [followers, setFollowers] = useState(0);
+  const [following, setFollowing] = useState(0);
+  const { userId } = useParams();
 
 
-    useEffect(() => {
-        async function getPostData() {
-                  const cleanedUserId = user.uid.replace(/^:/, "");
-            
-                  const postsRef = collection(db, "PostsMeta");
-                  const q = query(postsRef, where("userId", "==", cleanedUserId));
-                  const snapshot = await getDocs(q);
-                  const postsData = snapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                  }));
-                  setPosts(postsData);
+//   const user = useSelector(state => state.user.user);
 
-                  
-        }
-        getPostData();
-    }, [user.uid]);
-    console.log(posts)
-    return (
-        <div className="w-[80%] h-[10vh] flex ">
-        <div className="w-1/3 h-full flex items-center justify-center flex-col">
-            <h1 className="text-xl font-bold">{posts.length}</h1>
-            <p>Posts</p>
-        </div>
-        <div className="w-1/3 h-full flex items-center justify-center flex-col">
-            <h1 className="text-xl font-bold">0</h1>
-            <p>Followers</p>
-        </div>
-        <div className="w-1/3 h-full flex items-center justify-center flex-col">
-            <h1 className="text-xl font-bold">0</h1>
-            <p>Following</p>
-        </div>
+  useEffect(() => {
+    async function getData() {
+      if (!userId) return;
+
+      const cleanedUserId = userId.replace(/^:/, "");
+
+      // Posts
+      const postsRef = collection(db, "PostsMeta");
+      const postsQuery = query(postsRef, where("userId", "==", cleanedUserId));
+      const postsSnapshot = await getDocs(postsQuery);
+      setPosts(postsSnapshot.docs);
+
+      // Followers (ljudi koji prate korisnika)
+      const followersRef = collection(db, "followers");
+      const followersQuery = query(followersRef, where("followingId", "==", cleanedUserId));
+      const followersSnapshot = await getDocs(followersQuery);
+      setFollowers(followersSnapshot.size);
+
+      // Following (ljudi koje korisnik prati)
+      const followingQuery = query(followersRef, where("followerId", "==", cleanedUserId));
+      const followingSnapshot = await getDocs(followingQuery);
+      setFollowing(followingSnapshot.size);
+    }
+
+    getData();
+  }, [userId]);
+
+  return (
+    <div className="w-[80%] h-[10vh] flex">
+      <div className="w-1/3 h-full flex items-center justify-center flex-col">
+        <h1 className="text-xl font-bold">{posts.length}</h1>
+        <p>Posts</p>
       </div>
-    )
+      <div className="w-1/3 h-full flex items-center justify-center flex-col">
+        <h1 className="text-xl font-bold">{followers}</h1>
+        <p>Followers</p>
+      </div>
+      <div className="w-1/3 h-full flex items-center justify-center flex-col">
+        <h1 className="text-xl font-bold">{following}</h1>
+        <p>Following</p>
+      </div>
+    </div>
+  );
 }
